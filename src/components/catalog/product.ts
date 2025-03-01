0f9f5aa554d6d86156c978130f6a56cae9d4447d
teimosoRC <suinagato@gmail.com>
2025-03-01 15:24:03 +0300

    updated migrated

--- src/components/catalog/product.ts
+++ src/components/catalog/product.ts
@@ -1,10 +1,12 @@
-import { IProduct } from '../../types';
-
 import { TViewConstructionArgs, View } from '../base/view';
 
-type TProductRenderArgs = Pick<IProduct, 'image' | 'title' | 'category'> & {
-	price: string;
-	color: string | null;
+type TProductRenderArgs = {
+  index?: number;
+  title: string;
+  price: string;
+  category?: string;
+  image?: string;
+  color?: string | null;
 };
 
 type TProductEventHandlers = {
@@ -12,59 +14,71 @@ type TProductEventHandlers = {
 };
 
 class Product<
-	Element extends HTMLElement = HTMLElement,
-	RenderArgs extends object = TProductRenderArgs,
-	EventHandlers extends object = TProductEventHandlers
-> extends View<
-	Element,
-	RenderArgs & TProductRenderArgs,
-	EventHandlers & TProductEventHandlers
-> {
-	protected _titleElement: HTMLElement;
-	protected _categoryElement: HTMLElement;
-	protected _priceElement: HTMLElement;
-	protected _imageElement: HTMLImageElement;
-
-	constructor(args: TViewConstructionArgs<Element, EventHandlers>) {
-		super(args);
-
-		this._titleElement = this._element.querySelector('.card__title');
-		this._categoryElement = this._element.querySelector('.card__category');
-		this._priceElement = this._element.querySelector('.card__price');
-		this._imageElement = this._element.querySelector('.card__image');
-
-		if (this._eventHandlers.onClick instanceof Function) {
-			this._element.addEventListener('click', this._handleClick.bind(this));
+		Element extends HTMLElement = HTMLElement,
+		RenderArgs extends object = TProductRenderArgs,
+		EventHandlers extends object = TProductEventHandlers
+	> extends View<
+		Element, 
+		RenderArgs & TProductRenderArgs, 
+		EventHandlers & TProductEventHandlers
+	> {
+		protected _titleElement: HTMLElement;
+		protected _categoryElement: HTMLElement | null;
+		protected _priceElement: HTMLElement;
+		protected _imageElement: HTMLImageElement | null;
+		protected _indexElement: HTMLElement | null;
+		protected _buttonElement: HTMLButtonElement | null;
+	
+		constructor(args: TViewConstructionArgs<Element, EventHandlers>) {
+			super(args);
+	
+			this._titleElement = this._element.querySelector('.card__title');
+			this._categoryElement = this._element.querySelector('.card__category');
+			this._priceElement = this._element.querySelector('.card__price');
+			this._imageElement = this._element.querySelector('.card__image');
+			this._indexElement = this._element.querySelector('.basket__item-index');
+			this._buttonElement = this._element.querySelector('.card__button');
+			
+	
+			if (this._eventHandlers.onClick instanceof Function ) {
+				this._element.addEventListener('click', this._handleClick.bind(this));
+			}
 		}
-	}
-
-	set color(value: string | null) {
-		if (value) {
-			this._categoryElement.classList.add(`card__category_${value}`);
+		
+	
+		set index(value: number) {
+			if (this._indexElement) this._indexElement.textContent = String(value);
 		}
-	}
-
-	set category(value: string) {
-		this._categoryElement.textContent = String(value);
-	}
-
-	set price(value: string) {
-		this._priceElement.textContent = String(value);
-	}
-
-	set title(value: string) {
-		this._titleElement.textContent = String(value);
-	}
-
-	set image(value: string) {
-		this._imageElement.src = String(value);
-	}
-
-	protected _handleClick(event: MouseEvent) {
-		this._eventHandlers.onClick({
-			_event: event,
-		});
-	}
+	
+		set price(value: string) {
+			this._priceElement.textContent = String(value);
+		}
+	
+		set title(value: string) {
+			this._titleElement.textContent = String(value);
+		}
+	
+		set category(value: string) {
+			if (this._categoryElement) {
+				this._categoryElement.textContent = String(value);
+			}
+		}
+	
+		set image(value: string) {
+			if (this._imageElement) this._imageElement.src = String(value);
+		}
+	
+		set color(value: string | null) {
+			if (this._categoryElement && value) {
+				this._categoryElement.classList.add(`card__category_${value}`);
+			}
+		}
+	
+		protected _handleClick(event: MouseEvent) {
+			this._eventHandlers.onClick({
+				_event: event,
+			});
+		}
 }
 
 export { Product as ProductView, TProductRenderArgs, TProductEventHandlers };
--- src/components/global/page.ts
+++ src/components/global/page.ts
@@ -1,21 +1,78 @@
-import { TViewConstructionArgs, View } from '../base/view';
+import { TViewConstructionArgs, View, TViewNested} from '../base/view';
 
 type TPageRenderArgs = {
-	isLocked: boolean;
+	isLocked?: boolean;
+	counter?: number;
 };
 
-class Page extends View<HTMLElement, TPageRenderArgs> {
-	protected _wrapperElement: HTMLElement;
+type TProductsRenderArgs<T extends object> = {
+	items?: TViewNested<T>[];
+};
 
-	constructor(args: TViewConstructionArgs) {
+type TPageEventHandlers = {
+	onClick?: (args: { _event: MouseEvent }) => void;
+};
+
+class Page extends View<HTMLElement, TPageRenderArgs & TProductsRenderArgs<object>, TPageEventHandlers> {
+	protected _wrapperElement: HTMLElement;
+	protected _counterElement: HTMLElement;
+	protected _buttonElement: HTMLButtonElement;
+	protected _productsContainer: HTMLElement;
+
+	constructor(args: TViewConstructionArgs<HTMLElement, TPageEventHandlers>) {
 		super(args);
 
 		this._wrapperElement = this._element.querySelector('.page__wrapper');
+
+		this._counterElement = this._element.querySelector(
+			'.header__basket-counter'
+		);
+
+		this._buttonElement = this._element.querySelector('.header__basket');
+
+		this._productsContainer = this._element.querySelector('.gallery');
+
+		if (this._eventHandlers.onClick instanceof Function) {
+			this._buttonElement.addEventListener(
+				'click',
+				this._handleClick.bind(this)
+			);
+		}
 	}
 
 	set isLocked(value: boolean) {
 		this._wrapperElement.classList.toggle('page__wrapper_locked', value);
 	}
+
+	protected _handleClick(event: MouseEvent) {
+		this._eventHandlers.onClick({
+			_event: event,
+		});
+	}
+
+	set counter(value: number) {
+		this._counterElement.textContent = String(value);
+	}
+
+	set items(value: TViewNested[]) {
+		this._productsContainer.replaceChildren(
+			...value.map(({ view, renderArgs }) =>
+				view instanceof View ? view.render(renderArgs) : view
+			)
+		);
+	}
+
+	render<RenderArgs extends object = object>(
+		args: TPageRenderArgs & TProductsRenderArgs<RenderArgs>
+	) {
+		super.render(args);
+
+		if (args.items) {
+			this.items = args.items;
+		}
+
+		return this._element;
+	}
 }
 
-export { Page as PageView, TPageRenderArgs };
+export { Page as PageView, TPageRenderArgs, TPageEventHandlers, TProductsRenderArgs };
